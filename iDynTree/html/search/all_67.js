5866201c1e8e23092c3a23a51797b1bf1e7de762
pegua <pegua1@gmail.com>
2013-05-26 17:12:16 +0200

    Updated iDynTreeInterface

--- iDynTree/html/search/all_67.js
+++ iDynTree/html/search/all_67.js
@@ -1,9 +1,12 @@
 var searchData=
 [
-  ['getang',['getAng',['../classi_cub_1_1i_dyn_tree_1_1i_dyn_tree_interface.html#abe6667773d1eaa041bb128fed5075244',1,'iCub::iDynTree::iDynTreeInterface']]],
-  ['getd2ang',['getD2Ang',['../classi_cub_1_1i_dyn_tree_1_1i_dyn_tree_interface.html#ae6c5dc8e49c2e71745e97f645ceb55a8',1,'iCub::iDynTree::iDynTreeInterface']]],
-  ['getdang',['getDAng',['../classi_cub_1_1i_dyn_tree_1_1i_dyn_tree_interface.html#a88e6fcb3a458a5fe34d1dabb5d5f7cbb',1,'iCub::iDynTree::iDynTreeInterface']]],
+  ['getang',['getAng',['../classi_cub_1_1i_dyn_tree_1_1i_dyn_tree_interface.html#a74a4ee1a0f1b622d2297afdfde110c91',1,'iCub::iDynTree::iDynTreeInterface']]],
+  ['getcom',['getCOM',['../classi_cub_1_1i_dyn_tree_1_1i_dyn_tree_interface.html#a39bcd370f122b50906c7f094baa3f008',1,'iCub::iDynTree::iDynTreeInterface']]],
+  ['getcomjacobian',['getCOMJacobian',['../classi_cub_1_1i_dyn_tree_1_1i_dyn_tree_interface.html#a78db9b2f394c81140856e7e8da316e68',1,'iCub::iDynTree::iDynTreeInterface']]],
+  ['getcontacts',['getContacts',['../classi_cub_1_1i_dyn_tree_1_1i_dyn_tree_interface.html#a6a94633b6857377314a1ff1a2bca0e5f',1,'iCub::iDynTree::iDynTreeInterface']]],
+  ['getd2ang',['getD2Ang',['../classi_cub_1_1i_dyn_tree_1_1i_dyn_tree_interface.html#acc402fbb2abfd9a23e59bb683e9bdd8a',1,'iCub::iDynTree::iDynTreeInterface']]],
+  ['getdang',['getDAng',['../classi_cub_1_1i_dyn_tree_1_1i_dyn_tree_interface.html#aa559663eddb21bd5dc083e0000f42a9e',1,'iCub::iDynTree::iDynTreeInterface']]],
   ['getinertialmeasure',['getInertialMeasure',['../classi_cub_1_1i_dyn_tree_1_1i_dyn_tree_interface.html#ac0cb3b712cded5712e23497d172a4cc3',1,'iCub::iDynTree::iDynTreeInterface']]],
   ['getsensormeasurement',['getSensorMeasurement',['../classi_cub_1_1i_dyn_tree_1_1i_dyn_tree_interface.html#a4be85879e8234d362b6c6cf58b13ee59',1,'iCub::iDynTree::iDynTreeInterface']]],
-  ['gettorques',['getTorques',['../classi_cub_1_1i_dyn_tree_1_1i_dyn_tree_interface.html#abfd888d5d06f536c2680a334db6e1849',1,'iCub::iDynTree::iDynTreeInterface']]]
+  ['gettorques',['getTorques',['../classi_cub_1_1i_dyn_tree_1_1i_dyn_tree_interface.html#a39a98363539c4d5b231faa25938b4d7d',1,'iCub::iDynTree::iDynTreeInterface']]]
 ];
--- iDynTree/html/search/all_69.js
+++ iDynTree/html/search/all_69.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['idyntree',['iDynTree',['../group__i_dyn_tree.html',1,'']]],
-  ['idyntreeinterface',['iDynTreeInterface',['../classi_cub_1_1i_dyn_tree_1_1i_dyn_tree_interface.html',1,'iCub::iDynTree']]]
+  ['idyntreeinterface',['iDynTreeInterface',['../classi_cub_1_1i_dyn_tree_1_1i_dyn_tree_interface.html',1,'iCub::iDynTree']]],
+  ['idyntree',['iDynTree',['../index.html',1,'']]]
 ];
